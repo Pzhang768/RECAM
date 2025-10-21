15ea0b3cedf55079e51a1b52c79a94e6a6cab7c8
Paul Zhang <hzhang526@gmail.com>
2025-10-21 17:38:10 +1100

    feat(RECAM-2): Add Identity confguration and sql Dbcontext in program.cs

--- Remp.API/Program.cs
+++ Remp.API/Program.cs
@@ -1,6 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Remp.DataAccess;
+using Remp.Models.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Configure DbContext with SQL Server
+builder.Services.AddDbContext<RempDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Remp-SQLSERVER"),
+        sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()
+    )
+);
+
+// Configure Identity
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    // Password settings
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 8;
+
+    // Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+
+    // User settings
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<RempDbContext>()
+.AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -16,6 +49,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
